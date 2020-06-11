176f14c675517101c0a450b0b534262149244776
Et7f3 <cadeaudeelie@gmail.com>
2020-06-12 01:22:04 +0200

    feat: Add body-parser & show first routes.

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
+const layout = require("./utils/layout.js")
+
+const bodyParser = require('body-parser')
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+var myLogger = function (req, res, next) {
+    console.log(req.body);
+    next();
+};
+
+app.use(myLogger);
+
+app.post("/resto/login", require("./routes/resto/index.js"))
+app.get("/resto/login", require("./routes/resto/index.js"))
+app.get("/", require("./routes/resto/index.js"))
+app.get("/resto/commande", require("./routes/resto/commande.js"))
+app.post("/resto/commande", require("./routes/resto/commande.js"))
 
 app.listen(port, function () {
     console.log(`Listen on port ${port}`)
