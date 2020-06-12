7dc657289a596af8cebd858ee98da541dbb82977
Et7f3 <cadeaudeelie@gmail.com>
2020-06-12 05:04:56 +0200

    debug: rows empty ?

--- routes/admin/list.js
+++ routes/admin/list.js
@@ -4,7 +4,7 @@ const db = require("../../utils/db.js")
 module.exports = exports = function(req, res) {
     db.query("SELECT id, login FROM restaurateurs", (err, resul) => {
         console.error(err)
-        console.info(resul)
+        //console.info(resul)
         console.info(resul.rows)
         // TODO: handle err
         layout({title: "Listes des restaurateurs", "view": "admin/list.ejs", "data": {rows: resul.rows}}, req, res)
