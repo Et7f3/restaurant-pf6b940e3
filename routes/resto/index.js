1614d9f0baab2d4357e4ddc75cc043a7c6faf38f
Et7f3 <cadeaudeelie@gmail.com>
2020-06-12 05:31:02 +0200

    feat: check restaurateurs exist.

--- routes/resto/index.js
+++ routes/resto/index.js
@@ -2,11 +2,20 @@ const layout = require("../../utils/layout.js")
 
 module.exports = exports = function(req, res) {
     if (req.body.login && req.body.pass) {
-        res.writeHead(303, {
-            'Location': '/resto/commande'
-            //add other headers here...
-        });
-        res.end();
+        var args = [req.body.login, req.body.pass];
+        db.query("SELECT id, login FROM restaurateurs WHERE login=$1 AND pass=$2", args, (err, resul) => {
+            console.error(err)
+            // TODO: handle err
+            if (resul.rows.lenth == 1)
+            {
+                res.writeHead(303, {
+                    'Location': '/resto/commande'
+                    //add other headers here...
+                });
+                res.end();
+            } else
+                layout({title: "Connecter-vous", "view": "resto/index.ejs", "data": {message: "Mauvaise paire login/mot de passe."}}, req, res)
+        })
     } else
-        layout({title: "Connecter-vous", "view": "resto/index.ejs", "data": {}}, req, res)
+        layout({title: "Connecter-vous", "view": "resto/index.ejs", "data": {message: ""}}, req, res)
 }
