27bc03eb58e5a9e278221ad2e9a7897ab4744430
Et7f3 <cadeaudeelie@gmail.com>
2020-06-12 05:42:21 +0200

    feat: Add adresse field.

--- routes/resto/commande.js
+++ routes/resto/commande.js
@@ -2,19 +2,21 @@ const layout = require("../../utils/layout.js")
 
 module.exports = exports = function(req, res) {
     var data
-    if (req.body.tel && req.body.nom && req.body.prix) {
+    if (req.body.tel && req.body.nom && req.body.prix && req.body.adresse) {
         // TODO: save
-        console.log("save :", req.body.tel, req.body.nom, req.body.prix)
+        console.log("save :", req.body.tel, req.body.nom, req.body.prix, req.body.adresse)
         data = {
             tel: "",
             nom: "",
             prix: "",
+            adresse: "",
         }
     } else {
         data = {
             tel: req.body.tel,
             nom: req.body.nom,
             prix: req.body.prix,
+            adresse: req.body.adresse,
         }
     }
     layout({title: "Prendre une commande", "view": "resto/commande.ejs", "data": data}, req, res)
