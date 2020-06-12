5732bd6c61f440453c07299527f410cc57b1c251
Et7f3 <cadeaudeelie@gmail.com>
2020-06-12 03:45:27 +0200

    fix: Try to list users ?

--- routes/admin/list.js
+++ routes/admin/list.js
@@ -2,10 +2,10 @@ const layout = require("../../utils/layout.js")
 const db = require("../../utils/db.js")
 
 module.exports = exports = function(req, res) {
-    db.query("SELECT * FROM restaurateurs", (err, resul) => {
+    db.query("SELECT id, login FROM restaurateurs", (err, resul) => {
         console.error(err)
         console.info(resul)
         // TODO: handle err
-        layout({title: "Listes des restaurateurs", "view": "admin/list.ejs", "data": resul}, req, res)
+        layout({title: "Listes des restaurateurs", "view": "admin/list.ejs", "data": {rows: resul.rows}}, req, res)
     })
 }
