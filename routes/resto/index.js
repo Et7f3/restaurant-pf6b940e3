9d3d5243cd2c86c9077405352ef02b96e6bd5079
Et7f3 <cadeaudeelie@gmail.com>
2020-06-12 06:03:54 +0200

    Revert "fix: Add debug logs."
    This reverts commit 0b855a2ec62a173dc3e20606c516ba80e7017017.

--- routes/resto/index.js
+++ routes/resto/index.js
@@ -4,13 +4,9 @@ const db = require("../../utils/db.js")
 module.exports = exports = function(req, res) {
     if (req.body.login && req.body.pass) {
         var args = [req.body.login, req.body.pass];
-        console.log("on se co avec :", args)
         db.query("SELECT id, login FROM restaurateurs WHERE login=$1 AND pass=$2", args, (err, resul) => {
             console.error(err)
             // TODO: handle err
-            console.log(resul)
-            console.log(resul.rows)
-            console.log(resul.rows.length)
             if (resul.rows.length == 1)
             {
                 res.writeHead(303, {
