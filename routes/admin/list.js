e780299ce9613d8f0d192e99653d75cf593aab8c
Et7f3 <cadeaudeelie@gmail.com>
2020-06-12 04:04:14 +0200

    fix: more logs.

--- routes/admin/list.js
+++ routes/admin/list.js
@@ -5,6 +5,7 @@ module.exports = exports = function(req, res) {
     db.query("SELECT id, login FROM restaurateurs", (err, resul) => {
         console.error(err)
         console.info(resul)
+        console.info(resul.rows)
         // TODO: handle err
         layout({title: "Listes des restaurateurs", "view": "admin/list.ejs", "data": {rows: resul.rows}}, req, res)
     })
